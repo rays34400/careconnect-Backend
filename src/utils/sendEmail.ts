04fd096f4674892130900e8f59abd71e79040fd6
Sennia Rayan Sid-Ali <senniarayan1999@gmail.com>
2026-04-25 19:43:59 -0400

    verify email update new

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -12,7 +12,7 @@ export const sendVerificationEmail = async (
     },
   });
 
-  const verificationUrl = `careconnect-frontend-three.vercel.app/verify-email/${token}`;
+  const verificationUrl = `https://careconnect-frontend-three.vercel.app/verify-email/${token}`;
   await transporter.sendMail({
     from: `"CareConnect" <${process.env.EMAIL_USER}>`,
     to,
